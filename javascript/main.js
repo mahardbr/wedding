8d23e0d6ec4da6f6899a901a652555c9f63eaeb6
mahardbr <mahardbr@gmail.com>
2022-05-23 20:39:36 +0100

    updates

--- javascript/main.js
+++ javascript/main.js
@@ -16,15 +16,7 @@ var x = setInterval(function () {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML =
-    days +
-    " days " +
-    hours +
-    " hours " +
-    minutes +
-    " minutes and " +
-    seconds +
-    " seconds to go!!! ";
+  document.getElementById("demo").innerHTML = days + " days to go! ";
 
   // If the count down is over, write some text
   if (distance < 0) {
